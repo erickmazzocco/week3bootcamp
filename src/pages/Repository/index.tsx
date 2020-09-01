325013516b5648c5092eb65bb1f72bacc85b014c
Érick Mazzocco <erick.mazzocco@elogroup.com.br>
2020-09-01 16:43:14 -0300

    estilizando

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 interface RepositoryParams {
   repository: string;
@@ -10,10 +13,80 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>
-      Repository: {params.repository}
-    </h1>
-  );
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars3.githubusercontent.com/u/26230705?s=400&u=fdbc57fbb036367252d14a6554203e922f9a3eb7&v=4" alt="Érick Mazzocco" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="testeste">
+          <div>
+            <strong>asdasd</strong>
+            <p>assadd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+
+      <Issues>
+        <Link to="testeste">
+          <div>
+            <strong>asdasd</strong>
+            <p>assadd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+
+      <Issues>
+        <Link to="testeste">
+          <div>
+            <strong>asdasd</strong>
+            <p>assadd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+
+      <Issues>
+        <Link to="testeste">
+          <div>
+            <strong>asdasd</strong>
+            <p>assadd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  )
 };
 
 export default Repository;
