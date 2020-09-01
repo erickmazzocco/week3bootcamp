6b9b21cde2dab47f5d299e148fe60afa9b432072
Érick Mazzocco <erick.mazzocco@elogroup.com.br>
2020-09-01 14:04:28 -0300

    params

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
+import { Link } from 'react-router-dom'
 
 import { Title, Form, Repositories, Error } from './styles';
 
@@ -71,7 +72,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -81,7 +82,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
 
       </Repositories>
